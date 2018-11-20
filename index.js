f90a3a5e79c6092263b4441855c15b368abe25aa
BetyMehide <18532153+BetyMehide@users.noreply.github.com>
2018-11-19 16:04:35 -0800

    fixed errors

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var routes = require("./routes/routes.js");
+//var routes = require("./routes/routes.js");
 var app = express();
 const PORT = process.env.PORT
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-routes(app);
+app.get("/", function(req, res) {
+    res.status(200).send("Welcome to our restful API");
+  });
 
 var server = app.listen(PORT, function () {
     console.log("app running on port.", server.address().port);
